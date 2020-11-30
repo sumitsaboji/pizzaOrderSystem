5729d90e8e43b3b571ced25102f6fd77bfdbf5df
sushantsaboji <sushantsaboji@gmail.com>
2020-11-30 20:31:38 +0530

    added prod

--- PizzaOrderSystemApp/src/environments/environment.prod.ts
+++ PizzaOrderSystemApp/src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+   baseUrl : 'https://localhost:44370/api/',
+   
+   vegPizza : {
+    GetVegPizzaList: 'VegPizza/GetVegPizzaList',
+    GetCustomisePizzaMaster: 'VegPizza/GetCustomisePizzaMaster'
+  },
+  auth : {
+    login: 'Auth/Login',
+    register: 'Auth/Register',
+  } ,
+  Order : {
+    placeOrder: 'Order/PlaceOrder',
+    getOrders: 'Order/GetOrders'
+
+  }   
 };
